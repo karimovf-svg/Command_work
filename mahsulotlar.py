419d3af220a6b40bd2bcaf99dba8e8ff6f63e395
azamat <azamatbekxoshimov571>
2024-12-20 08:02:50 +0500

    market end

--- mahsulotlar.py
+++ mahsulotlar.py
@@ -1,4 +1,3 @@
-"""Azamat"""
 class Mahsulotlar:
     def __init__(self, nomi, narx, muddat):
         self.nomi = nomi
@@ -17,4 +16,5 @@ tavar7 = Mahsulotlar("cola", 15000, "02/09/2025")
 tavar8 = Mahsulotlar("shokalad", 30000, "02/12/2025")
 tavar9 = Mahsulotlar("guruch", 25000, "02/01/2026")
 tavar10 = Mahsulotlar("shakar", 14000, "02/03/2026")
+
 tavarlar = [tavar1, tavar2, tavar3, tavar4, tavar5, tavar6, tavar7, tavar8, tavar9, tavar10]
